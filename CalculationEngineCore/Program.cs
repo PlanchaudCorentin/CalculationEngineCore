cbc906ecfae9b8be12a287169121aaf7777caae7
Corentin <corentin.planchaud@viacesi.fr>
2019-06-24 12:19:25 +0200

    initial commit

---
+++ CalculationEngineCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CalculationEngineCoreLibrary;
+
+namespace CalculationEngineCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connector conn = new Connector("admin", "devproject", "/", "192.168.43.88");
+        }
+    }
+}
---
+++ CalculationEngineCoreLibrary/Connector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace CalculationEngineCoreLibrary
+{
+    public class Connector
+    {
+        private IConnection conn;
+        private IModel channel;
+
+        public Connector(string user, string pass, string vhost, string hostname)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = hostname;
+            factory.VirtualHost = vhost;
+            factory.Password = pass;
+            factory.UserName = user;
+            factory.Port = 5672;
+
+            this.conn = factory.CreateConnection();
+
+            this.channel = conn.CreateModel();
+            channel.QueueDeclare("Test", true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var body = ea.Body;
+                Console.WriteLine(System.Text.Encoding.Default.GetString(body));
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+            string consumerTag = channel.BasicConsume("Test", false, consumer);
+            
+            
+
+        }
+
+    }
+}
